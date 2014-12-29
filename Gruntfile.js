697336f972f54aab809980345b164e6a8ca50457
qq2441570482 <2441570482@qq.com>
2014-12-29 14:22:55 +0800

    [yanqing] add grunt build tool into project

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt){
+
+    // 项目配置
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                //src: 'src/<%=pkg.name %>.js',
+                src: './app/assets/javascripts/<%=pkg.name %>.js',
+                //dest: 'build/<%= pkg.name %>.min.js'
+                dest: './app/assets/javascripts/<%= pkg.name %>.min.js'
+            }               
+        }
+    });
+
+    // 加载提供"uglify"任务的插件
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // 默认任务
+    grunt.registerTask('default', ['uglify']);
+}

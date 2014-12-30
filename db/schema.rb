ea97627f94ce3f359eb7c1d03867b88cbcba5b47
qq2441570482 <2441570482@qq.com>
2014-12-30 18:34:58 +0800

    [yanqing] edit the login page use usename replace email field  and change the show language

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229094653) do
+ActiveRecord::Schema.define(version: 20141230101451) do
 
   create_table "users", force: true do |t|
-    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email",                  default: "", null: false
@@ -27,6 +26,7 @@ ActiveRecord::Schema.define(version: 20141229094653) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

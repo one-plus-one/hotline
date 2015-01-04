7915556a19bc0c69beee93ab3dadd38548dd3143
wubang <wolfwubang@gmail.com>
2015-01-04 11:57:24 +0800

    [bangwu] add index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   devise_for :users
   resources :users
 
-  root to: "users#index"
+  root to: "static_pages#home"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

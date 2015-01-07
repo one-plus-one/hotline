2ba079688ddabc014f5d9db58f5fb1f48dd98f26
Qingsong You <yqsqq@163.com>
2015-01-07 10:40:44 +0800

    [story-18][Qingsong] update the login form to support the screen reader

--- app/js/home/main.js
+++ app/js/home/main.js
@@ -1,43 +1,42 @@
+function ws_connect() {
+    if ("WebSocket" in window) {
+        var ws = new WebSocket("ws://localhost:8080/echo");
+        ws.onopen = function (event) {
+            log.innerHTML +=
+                "<div class='green'>websocket connected</div>";
+            //connect.style.display = 'none';
+            //tools.style.display = 'block';
+        };
 
-function ws_connect(){
-	if ("WebSocket" in window) {
-				var ws = new WebSocket("ws://localhost:8080/echo");
-				ws.onopen = function(event) {
-					log.innerHTML+=
-						"<div class='green'>websocket connected</div>";
-					//connect.style.display = 'none';
-					//tools.style.display = 'block';
-				};
+        ws.onerror = function (event) {
+            log.innerHTML +=
+                "<div class='red'>error on websocket!</div>";
+        };
 
-				ws.onerror = function(event) {
-					log.innerHTML+=
-						"<div class='red'>error on websocket!</div>";
-				};
-
-				ws.onmessage = function(event) {
-					log.innerHTML+=
-						"<div>get message:<span class='data'>"+event.data+"</span></div>";
-          var data=event.data.split(",");
-          var ani=data[6].split(":")[1];
-          var status=data[3].split(":")[1];
-          if(status=='ringing'){
-            location.href = 'static_pages/oncall?tel='+ani;
-          }
-				};
-
-				ws.onclose = function(event) {
-					log.innerHTML+=
-						"<div class='red'>websocket closed</div>";
-					//connect.style.display = 'block';
-					tools.style.display = 'none';
-				};
-			} else {
-				// the browser doesn't support WebSocket
-				alert("WebSocket NOT supported here!\r\n\r\nBrowser: " +
-					navigator.appName + " " + navigator.appVersion);
+        ws.onmessage = function (event) {
+            log.innerHTML +=
+                "<div>get message:<span class='data'>" + event.data + "</span></div>";
+            var data = event.data.split(",");
+            var ani = data[6].split(":")[1];
+            var status = data[3].split(":")[1];
+            if (status == 'ringing') {
+                location.href = 'static_pages/oncall?tel=' + ani;
             }
-        return false;
-    };
+        };
+
+        ws.onclose = function (event) {
+            log.innerHTML +=
+                "<div class='red'>websocket closed</div>";
+            //connect.style.display = 'block';
+            tools.style.display = 'none';
+        };
+    } else {
+        // the browser doesn't support WebSocket
+        alert("WebSocket NOT supported here!\r\n\r\nBrowser: " +
+        navigator.appName + " " + navigator.appVersion);
+    }
+    return false;
+};
 
 function ws_disconnect() {
     ws.close();
@@ -46,72 +45,72 @@ function ws_disconnect() {
 }
 
 function ws_send_data() {
-    log.innerHTML+=
-        "<div>send message:<span class='data'>"+val.value+"</span></div>";
+    log.innerHTML +=
+        "<div>send message:<span class='data'>" + val.value + "</span></div>";
     ws.send(val.value);
 
     return false;
 }
 
-function login(){
-    location.href="wisexclient:login?agentid=1109&passwd=Hanwin@20!4&name=Johnson";
+function login() {
+    location.href = "wisexclient:login?agentid=1109&passwd=Hanwin@20!4&name=Johnson";
 }
-function dial(){
-    location.href="wisexclient:dial?ani=31119104&dnis=718930430189";
-}
-
-function offhook(){
-    location.href="wisexclient:offhook";
+function dial() {
+    location.href = "wisexclient:dial?ani=31119104&dnis=718930430189";
 }
 
-function onhook(){
-    location.href="wisexclient:onhook";
+function offhook() {
+    location.href = "wisexclient:offhook";
 }
 
-function  setbusy(){
-    location.href="wisexclient:park";
+function onhook() {
+    location.href = "wisexclient:onhook";
 }
 
-function  setready(){
-    location.href="wisexclient:setready";
+function setbusy() {
+    location.href = "wisexclient:park";
 }
 
-function hold(){
-    location.href="wisexclient:hold";
+function setready() {
+    location.href = "wisexclient:setready";
 }
 
-function unhold(){
-    location.href="wisexclient:unhold";
+function hold() {
+    location.href = "wisexclient:hold";
 }
 
-function blindtransfer(){
-    location.href="wisexclient:blindtransfer?transferee=1000";
+function unhold() {
+    location.href = "wisexclient:unhold";
 }
 
-function dialthird(){
-    location.href="wisexclient:dial?ani=31119105&dnis=718901700171";
+function blindtransfer() {
+    location.href = "wisexclient:blindtransfer?transferee=1000";
 }
 
-function alternate(){
-    location.href="wisexclient:alternate";
+function dialthird() {
+    location.href = "wisexclient:dial?ani=31119105&dnis=718901700171";
 }
 
-function hungupthird(){
-    location.href="wisexclient:hungupthird";
+function alternate() {
+    location.href = "wisexclient:alternate";
 }
 
-function  transfer(){
-    location.href="wisexclient:transfer";
+function hungupthird() {
+    location.href = "wisexclient:hungupthird";
 }
 
-function  startconference(){
-    location.href="wisexclient:startconference?ani=31119127&third_party=718901700171";
+function transfer() {
+    location.href = "wisexclient:transfer";
 }
 
-function logout(){
-    location.href="wisexclient:logout";
+function startconference() {
+    location.href = "wisexclient:startconference?ani=31119127&third_party=718901700171";
 }
 
-function getagentstatus(){
-    location.href="wisexclient:getagentstatus";
+function logout() {
+    location.href = "wisexclient:logout";
+}
+
+function getagentstatus() {
+    location.href = "wisexclient:getagentstatus";
 }

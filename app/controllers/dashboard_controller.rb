35dc02b748f3162bf191ebf0a6026dd33e628b75
Qingsong You <yqsqq@163.com>
2015-01-12 15:28:04 +0800

    [Wubang,Yanqing,Qingsong] fix the i18n issue on the workorder page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -19,21 +19,19 @@ class DashboardController < ApplicationController
       @customer.phone_num=params[:tel]
       @customer.save!
     end
-
-
   end
 
   def create
-    customer  = Customer.find_by_id(params[:dashboard][:id])
-    customer.save(params[:dashboard])
+    customer  = Customer.find_by_id(params[:workorder][:id])
+    customer.save(params[:workorder])
 
     #创建工单
     workorder = WorkOrder.new
-    workorder.save(params[:dashboard],customer,current_user)
+    workorder.save(params[:workorder],customer,current_user)
 
     # 创建问题表
     customerQestion = CustomerQuestion.new
-    customerQestion.save(params[:dashboard],workorder)
+    customerQestion.save(params[:workorder],workorder)
 
     redirect_to dashboard_home_path
   end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,4 +1,6 @@
 # in config/initializers/locale.rb
 
 # tell the I18n library where to find your translations
-I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{zh,yml}')]
+I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{zh-CN,yml}')]
+I18n.config.enforce_available_locales = false
+I18n.default_locale = 'zh-CN'

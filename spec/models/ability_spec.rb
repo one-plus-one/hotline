4fa17061f71b83ea175d294813b894895958c73c
wubang <wolfwubang@gmail.com>
2015-01-23 11:39:26 +0800

    [bangwu] finish the ability rspec test

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,13 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe Ability do |app|
+RSpec.describe Ability do
 
   describe 'ability' do
 
     it 'should permit System user manage all' do
       user = FactoryGirl.create :user
+      user.add_role 'system'
       ability = Ability.new(user)
-      puts user.id
+      expect(ability.can? :manager, :all).to eql(true)
+      end
+    it 'should permit admin user read all' do
+      user = FactoryGirl.create :user
+      user.add_role 'admin'
+      ability = Ability.new(user)
+      expect(ability.can? :manager, :all).to eql(false)
+      expect(ability.can? :read, :all).to eql(true)
+    end
+
+    it 'should permit admin user read all' do
+      user = FactoryGirl.create :user
+      user.add_role 'guest'
+      ability = Ability.new(user)
+      expect(ability.can? :manager, :all).to eql(false)
+      expect(ability.can? :read, :all).to eql(false)
+      expect(ability.can? :read, :work_orders).to eql(true)
     end
   end
 end

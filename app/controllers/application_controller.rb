54f0d73b05bc15dcbc72758381e4e9e8f85a0b2f
wubang <wolfwubang@gmail.com>
2015-01-23 09:55:59 +0800

    [bangwu] remove binding.pry from application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
-      binding.pry
       if resource.id == 1
         resource.add_role 'system'
         binding.pry

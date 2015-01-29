036e80b901606a58e5c2b26a1f6503317ed11823
qq2441570482 <2441570482@qq.com>
2015-01-29 11:00:50 +0800

    [story-34][Yanqing]add i18n

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,5 +1,7 @@
 class RepositoriesController < ApplicationController
   before_action :new_a_repository, only: [:new,:create]
+  before_action :find_a_repository, only: [:show,:update,:destroy]
+
   def index
     @repositories = Repository.page(params[:page]).per(50)
   end
@@ -22,26 +24,28 @@ class RepositoriesController < ApplicationController
   end
 
   def show
-    @repository = Repository.find(params[:id])
     @catalogs = Catalog.all
   end
 
   def update
-    @repository = Repository.find(params[:id])
     @repository.save(params)
     redirect_to repositories_path
   end
 
   def destroy
-    repository = Repository.find(params[:id])
-    repository.destroy!
+    @repository.destroy!
     @repository_log = RepositoryLog.new
     event = '知识库表中问题为:' + repository.title + '的记录'
     @repository_log.save(current_user.username,'删除',event)
     redirect_to repositories_path
   end
 
-  def new_a_repository
-    @repository = Repository.new
-  end
+  private
+    def new_a_repository
+      @repository = Repository.new
+    end
+
+    def find_a_repository
+      @repository = Repository.find(params[:id])
+    end
 end
